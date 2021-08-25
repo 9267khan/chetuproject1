07f3ec20198a9418a997abd3e5ce2bd01e84423b
Shakir khan <70830095+9267khan@users.noreply.github.com>
2021-08-25 10:10:54 +0530

    Add files via upload

---
+++ chetupro1JS.js
@@ -0,0 +1,44 @@
+function red1(){
+document.getElementById("div2").style.color="red";
+}
+document.getElementById("h1").addEventListener("click",red1);
+
+function green2(){
+    document.getElementById("div2").style.color="green";
+    }
+    document.getElementById("h2").addEventListener("click",green2);
+    
+    function yellow3(){
+        document.getElementById("div2").style.color="yellow";
+        }
+        document.getElementById("h3").addEventListener("click",yellow3);
+         
+        function fs20(){
+            document.getElementById("div2").style.fontSize="20px";
+            }
+            document.getElementById("h4").addEventListener("click",fs20);
+           
+            function fs30(){
+                document.getElementById("div2").style.fontSize="30px";
+                }
+                document.getElementById("h5").addEventListener("click",fs30);
+                
+                function fs40(){
+                    document.getElementById("div2").style.fontSize="40px";
+                    }
+                    document.getElementById("h6").addEventListener("click",fs40);
+                    
+                    function cursive(){
+                        document.getElementById("div2").style.fontFamily="cursive";
+                        }
+                        document.getElementById("h7").addEventListener("click",cursive);
+                        
+                        function fantasy(){
+                            document.getElementById("div2").style.fontFamily="fantasy";
+                            }
+                            document.getElementById("h8").addEventListener("click",fantasy);
+                            
+                            function monospace(){
+                                document.getElementById("div2").style.fontFamily="monospace";
+                                }
+                                document.getElementById("h9").addEventListener("click",monospace);
